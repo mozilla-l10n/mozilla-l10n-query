0b8afd10c227103bd32854d9d35900d7d4b4e4b9
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-09-17 16:46:34 +0200

    Add tests

---
+++ tests/units/QueryL10n/Repositories.php
@@ -0,0 +1,91 @@
+<?php
+namespace tests\units\QueryL10n;
+
+use atoum;
+use QueryL10n\Repositories as _Repositories;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+class Repositories extends atoum\test
+{
+    public function testGetRepositories()
+    {
+        $obj = new _Repositories(TEST_FILES);
+
+        $result = [
+            [
+                "id" => "beta",
+                "name"  => "Beta",
+                "locales" => ['da', 'de', 'dsb', 'el']
+            ],
+            [
+                "id" => "gaia_1_3",
+                "name"  => "Gaia 1.3",
+                "locales" => ['ar', 'bg', 'bn-BD', 'ca', 'cs']
+            ],
+        ];
+
+        $this
+            ->array($obj->getRepositories())
+                ->isEqualTo($result);
+    }
+
+    public function testGetGaiaRepositories()
+    {
+        $obj = new _Repositories(TEST_FILES);
+
+        $result = [
+            [
+                "id" => "gaia_1_3",
+                "name"  => "Gaia 1.3",
+                "locales" => ['ar', 'bg', 'bn-BD', 'ca', 'cs']
+            ],
+        ];
+
+        $this
+            ->array($obj->getGaiaRepositories())
+                ->isEqualTo($result);
+    }
+
+    public function testGetSingleRepository()
+    {
+        $obj = new _Repositories(TEST_FILES);
+
+        $result = [
+                "id" => "gaia_1_3",
+                "name"  => "Gaia 1.3",
+                "locales" => ['ar', 'bg', 'bn-BD', 'ca', 'cs']
+        ];
+
+        $this
+            ->array($obj->getSingleRepository('gaia_1_3'))
+                ->isEqualTo($result);
+
+        // Non existing repository
+        $this
+            ->boolean($obj->getSingleRepository('foobar'))
+                ->isFalse();
+    }
+
+    public function getSupportedLocalesDP()
+    {
+        return [
+            ['gaia_1_3', ['ar', 'bg', 'bn-BD', 'ca', 'cs']],
+            ['beta', ['da', 'de', 'dsb', 'el']],
+            ['foobar', []]
+        ];
+    }
+
+    /**
+     * @dataProvider getSupportedLocalesDP
+     */
+    public function testGetSupportedLocales($a, $b)
+    {
+        $obj = new _Repositories(TEST_FILES);
+
+        $this
+            ->array($obj->getSupportedLocales($a))
+                ->isEqualTo($b);
+    }
+}
+
