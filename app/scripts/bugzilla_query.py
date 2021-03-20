5d36d83cbb2e5ca960bed10a03ddc1d02c7d38da
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2021-03-20 06:11:51 +0100

    Reformat with Black

--- app/scripts/bugzilla_query.py
+++ app/scripts/bugzilla_query.py
@@ -8,78 +8,76 @@ from urllib.request import urlopen
 
 
 def nested_dict():
-    '''
-        Create a dictionary that auto-generates keys when trying to set a
-        a value for a key that doesn't exist (no need to check for its
-        existence)
-    '''
+    """
+    Create a dictionary that auto-generates keys when trying to set a
+    a value for a key that doesn't exist (no need to check for its
+    existence)
+    """
     return collections.defaultdict(nested_dict)
 
 
 def main():
     # Get absolute path of ../sources from current script location (not current folder)
     sources_folder = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), os.pardir, 'sources'))
-    output_filename = os.path.join(sources_folder, 'bugzilla_components.json')
+        os.path.join(os.path.dirname(__file__), os.pardir, "sources")
+    )
+    output_filename = os.path.join(sources_folder, "bugzilla_components.json")
     json_components = nested_dict()
 
     # Get list of locales for field cf_locale, used for example on www.mozilla.org
     # Unescaped URL: https://bugzilla.mozilla.org/jsonrpc.cgi?method=Bug.fields&params=[ { 'names': ['cf_locale'] } ]
-    json_url = 'https://bugzilla.mozilla.org/jsonrpc.cgi?method=Bug.fields&params=%5B%20{%20%22names%22:%20%5B%22cf_locale%22%5D%20}%20%5D'
+    json_url = "https://bugzilla.mozilla.org/jsonrpc.cgi?method=Bug.fields&params=%5B%20{%20%22names%22:%20%5B%22cf_locale%22%5D%20}%20%5D"
     try:
         response = urlopen(json_url)
         json_data = json.load(response)
         try:
-            for component in json_data['result']['fields'][0]['values']:
-                locale = component['name'].partition('/')[0].strip()
-                json_components['cf_locale'][locale] = (
-                    {
-                        'full_name': component['name'],
-                        'description': component['name'].partition('/')[2].strip()
-                    }
-                )
+            for component in json_data["result"]["fields"][0]["values"]:
+                locale = component["name"].partition("/")[0].strip()
+                json_components["cf_locale"][locale] = {
+                    "full_name": component["name"],
+                    "description": component["name"].partition("/")[2].strip(),
+                }
 
         except Exception as e:
-            print('Error extracting data from json response')
+            print("Error extracting data from json response")
             print(e)
     except Exception as e:
-        print('Error reading json reply from {}'.format(json_url))
+        print("Error reading json reply from {}".format(json_url))
         print(e)
 
     # Get list of components of the Mozilla Localizations product
     # Unescaped URL: https://bugzilla.mozilla.org/jsonrpc.cgi?method=Product.get&params=[ { 'names': ['Mozilla Localizations'] } ]
-    json_url = 'https://bugzilla.mozilla.org/jsonrpc.cgi?method=Product.get&params=[%20{%20%22names%22:%20[%22Mozilla%20Localizations%22]%20}%20]'
+    json_url = "https://bugzilla.mozilla.org/jsonrpc.cgi?method=Product.get&params=[%20{%20%22names%22:%20[%22Mozilla%20Localizations%22]%20}%20]"
     try:
         response = urlopen(json_url)
         json_data = json.load(response)
         try:
-            for component in json_data['result']['products'][0]['components']:
-                locale = component['name'].partition('/')[0].strip()
+            for component in json_data["result"]["products"][0]["components"]:
+                locale = component["name"].partition("/")[0].strip()
                 excluded_components = [
-                    'Documentation', 'Infrastructure',
-                    'Other', 'Registration & Management'
+                    "Documentation",
+                    "Infrastructure",
+                    "Other",
+                    "Registration & Management",
                 ]
                 if locale not in excluded_components:
-                    json_components['mozilla_localizations'][locale] = (
-                        {
-                            'full_name': component['name'],
-                            'description': component['name'].partition('/')[2].strip()
-                        }
-                    )
+                    json_components["mozilla_localizations"][locale] = {
+                        "full_name": component["name"],
+                        "description": component["name"].partition("/")[2].strip(),
+                    }
 
         except Exception as e:
-            print('Error extracting data from json response')
+            print("Error extracting data from json response")
             print(e)
     except Exception as e:
-        print('Error reading json reply from {}'.format(json_url))
+        print("Error reading json reply from {}".format(json_url))
         print(e)
 
     # Write list of components name
-    cache_file = open(output_filename, 'w')
-    cache_file.write(json.dumps(
-        json_components, sort_keys=True, indent=2))
+    cache_file = open(output_filename, "w")
+    cache_file.write(json.dumps(json_components, sort_keys=True, indent=2))
     cache_file.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- app/scripts/update_sources.py
+++ app/scripts/update_sources.py
@@ -7,47 +7,48 @@ from urllib.parse import quote as urlquote
 from urllib.request import urlopen
 
 
-def saveTextFile(sources_folder, filename, locales, subfolder=''):
-    print('Writing file {}.txt'.format(filename))
-    if subfolder != '':
+def saveTextFile(sources_folder, filename, locales, subfolder=""):
+    print("Writing file {}.txt".format(filename))
+    if subfolder != "":
         sources_folder = os.path.join(sources_folder, subfolder)
-    output_file = open(os.path.join(sources_folder, '{}.txt'.format(filename)), 'w')
+    output_file = open(os.path.join(sources_folder, "{}.txt".format(filename)), "w")
     for locale in locales:
-        output_file.write('{}\n'.format(locale))
+        output_file.write("{}\n".format(locale))
     output_file.close()
 
 
 def main():
     update_sources = {
-        'central': {
-            'sources': [
-                'https://hg.mozilla.org/mozilla-central/raw-file/default/browser/locales/all-locales',
+        "central": {
+            "sources": [
+                "https://hg.mozilla.org/mozilla-central/raw-file/default/browser/locales/all-locales",
             ],
-            'filename': 'central',
-            'format': 'txt',
-            'gecko_strings': True,
+            "filename": "central",
+            "format": "txt",
+            "gecko_strings": True,
         },
-        'beta': {
-            'sources': [
-                'https://hg.mozilla.org/releases/mozilla-beta/raw-file/default/browser/locales/shipped-locales',
+        "beta": {
+            "sources": [
+                "https://hg.mozilla.org/releases/mozilla-beta/raw-file/default/browser/locales/shipped-locales",
             ],
-            'filename': 'beta',
-            'format': 'txt',
-            'gecko_strings': True,
+            "filename": "beta",
+            "format": "txt",
+            "gecko_strings": True,
         },
-        'release': {
-            'sources': [
-                'https://hg.mozilla.org/releases/mozilla-release/raw-file/default/browser/locales/shipped-locales',
+        "release": {
+            "sources": [
+                "https://hg.mozilla.org/releases/mozilla-release/raw-file/default/browser/locales/shipped-locales",
             ],
-            'filename': 'release',
-            'format': 'txt',
-            'gecko_strings': True,
+            "filename": "release",
+            "format": "txt",
+            "gecko_strings": True,
         },
     }
 
     # Get absolute path of ../sources from current script location (not current folder)
     sources_folder = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), os.pardir, 'sources'))
+        os.path.join(os.path.dirname(__file__), os.pardir, "sources")
+    )
 
     # Gecko-strings channel is a special repository including all locales
     # shipping across all the branches. It also includes en-US
@@ -56,11 +57,11 @@ def main():
     # Query Pontoon API to find locales supported in desktop products and
     # mozilla.org
     pontoon_locales = {
-        'pontoon': [],
-        'pontoon-mozorg': [],
+        "pontoon": [],
+        "pontoon-mozorg": [],
     }
 
-    query = '''
+    query = """
 {
   firefox: project(slug: "firefox") {
     ...allLocales
@@ -89,33 +90,35 @@ fragment allLocales on Project {
     }
   }
 }
-'''
+"""
     try:
-        url = 'https://pontoon.mozilla.org/graphql?query={}'.format(urlquote(query))
-        print('Reading sources for Pontoon')
+        url = "https://pontoon.mozilla.org/graphql?query={}".format(urlquote(query))
+        print("Reading sources for Pontoon")
         response = urlopen(url)
         json_data = json.load(response)
-        for project, project_data in json_data['data'].items():
-            pontoon_bucket = 'pontoon-mozorg' if project == 'mozilla_org' else 'pontoon'
-            for element in project_data['localizations']:
-                code = element['locale']['code']
+        for project, project_data in json_data["data"].items():
+            pontoon_bucket = "pontoon-mozorg" if project == "mozilla_org" else "pontoon"
+            for element in project_data["localizations"]:
+                code = element["locale"]["code"]
                 if code not in pontoon_locales[pontoon_bucket]:
                     pontoon_locales[pontoon_bucket].append(code)
 
         # Store locales for projects in Pontoon
-        projects = list(json_data['data'].keys())
+        projects = list(json_data["data"].keys())
         output = {}
         for project in projects:
             # Ignore Firefox, since the list is coming from hg
-            if project == 'firefox':
+            if project == "firefox":
                 continue
 
             # Need to group different projects for android-l10n
-            project_dest = "android_l10n" if project.startswith("android_l10n") else project
+            project_dest = (
+                "android_l10n" if project.startswith("android_l10n") else project
+            )
 
             locales = []
-            for element in json_data['data'][project_dest]['localizations']:
-                locales.append(element['locale']['code'])
+            for element in json_data["data"][project_dest]["localizations"]:
+                locales.append(element["locale"]["code"])
             locales.sort()
 
             if project_dest not in output:
@@ -129,38 +132,38 @@ fragment allLocales on Project {
 
     for filename, locales in pontoon_locales.items():
         locales.sort()
-        saveTextFile(sources_folder, filename, locales, 'tools')
+        saveTextFile(sources_folder, filename, locales, "tools")
 
     for id, update_source in update_sources.items():
         supported_locales = []
-        for url in update_source['sources']:
-            print('Reading sources for {} from {}'.format(id, url))
+        for url in update_source["sources"]:
+            print("Reading sources for {} from {}".format(id, url))
             response = urlopen(url)
-            if update_source['format'] == 'txt':
+            if update_source["format"] == "txt":
                 for locale in response:
                     locale = locale.rstrip().decode()
-                    if locale not in ['', 'en-US'] and locale not in supported_locales:
+                    if locale not in ["", "en-US"] and locale not in supported_locales:
                         supported_locales.append(locale)
             else:
                 json_data = json.load(response)
                 for locale in json_data:
                     locale = locale.rstrip()
-                    if locale != '' and locale not in supported_locales:
+                    if locale != "" and locale not in supported_locales:
                         supported_locales.append(locale)
         # Sort locales
         supported_locales.sort()
-        if update_source['gecko_strings']:
+        if update_source["gecko_strings"]:
             gecko_strings_locales += supported_locales
 
         # Write back txt file
-        saveTextFile(sources_folder, update_source['filename'], supported_locales)
+        saveTextFile(sources_folder, update_source["filename"], supported_locales)
 
     # Output gecko-strings locales
     gecko_strings_locales = list(set(gecko_strings_locales))
-    gecko_strings_locales.append('en-US')
+    gecko_strings_locales.append("en-US")
     gecko_strings_locales.sort()
-    saveTextFile(sources_folder, 'gecko_strings', gecko_strings_locales)
+    saveTextFile(sources_folder, "gecko_strings", gecko_strings_locales)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
